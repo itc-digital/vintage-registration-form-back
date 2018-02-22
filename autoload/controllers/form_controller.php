a1fbe9c7b29c98e55befeacca264ddfc06e1e33a
wanku-x <wanku@bk.ru>
2018-02-23 02:17:08 +0300

    Потерялся факультет, проверяю

--- autoload/controllers/form_controller.php
+++ autoload/controllers/form_controller.php
@@ -5,7 +5,7 @@ class form_controller {
     public function handle_data($f3)
     {
         if (\models\form::validate_post($f3)) {
-            print_r($f3->get('POST'));
+            print_r($f3->get('POST')[faculty]);
             //\models\form::create_record($f3);
             $f3->status(200);
         } else {

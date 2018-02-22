31d4cd5ea98ecce532e9232fcd0980e2c3f35e97
wanku-x <wanku@bk.ru>
2018-02-23 01:53:12 +0300

    Завершение бэкэнда

--- autoload/models/form.php
+++ autoload/models/form.php
@@ -2,6 +2,15 @@
 namespace models;
 
 class form {
+    public static function save_screenshot($files, $destination)
+    {
+        $info = new \SplFileInfo($files[screenshot][name]);
+        $file_name = md5(microtime()).'.'.$info->getExtension(); 
+        $dir = "/home/s/slink21/front/public_html/".$destination;
+        move_uploaded_file($files[screenshot][tmp_name], $dir.$file_name);
+        return $file_name;
+    }
+
     public static function validate_post($f3)
     {
         $data = $f3->get('POST');
@@ -22,6 +31,19 @@ class form {
 
     public static function create_record($f3)
     {
-        print_r($f3->get('FILES'));
+        $data = $f3->get('POST');
+        $files = $f3->get('FILES');
+
+        $file_name = form::save_screenshot($files, 'ui/photos/');
+
+        $f3->get('DB')->exec('INSERT INTO `users`(`id`, `firstname`, `lastname`, `github`, `telegram`, `screenshot`) VALUES (?,?,?,?,?,?)', 
+		array(
+            1=> NULL,
+			2=> $data[firstname],
+			3=> $data[lastname],
+			4=> $data[github],
+			5=> $data[telegram],
+			6=> '/ui/miss/photos/'.$file_name
+		));
     }
 }
--- index.php
+++ index.php
@@ -3,4 +3,12 @@
 $f3=require('lib/base.php');
 $f3->config('config.ini');
 $f3->config('routes.ini');
+
+$db = new DB\SQL(
+	'mysql:host='.$f3->get('db_host').';port=3306;dbname='.$f3->get('db_name'),
+	$f3->get('db_user'),
+	$f3->get('db_pwd')
+);
+$f3->set('DB', $db);
+
 $f3->run();

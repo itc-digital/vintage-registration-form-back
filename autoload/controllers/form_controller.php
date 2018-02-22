a663f6659b45606507e4d6afe20f940d342dbf31
wanku-x <wanku@bk.ru>
2018-02-23 00:29:58 +0300

    Инициализация функций validate и create_record

--- autoload/controllers/form_controller.php
+++ autoload/controllers/form_controller.php
@@ -4,6 +4,11 @@ namespace controllers;
 class form_controller {
     public function handle_data($f3)
     {
-        $f3->status(200);
+        if (\models\form::validate($f3)) {
+            \models\form::create_record($f3);
+            $f3->status(200);
+        } else {
+            $f3->status(418);
+        }
     }
 }
---
+++ autoload/models/form.php
@@ -0,0 +1,14 @@
+<?php
+namespace models;
+
+class form {
+    public static function validate($f3)
+    {
+        return TRUE;
+    }
+
+    public static function create_record($f3)
+    {
+
+    }
+}

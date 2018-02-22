c6be14e170fb0e135502eb57fc795d33be769c61
wanku-x <wanku@bk.ru>
2018-02-23 01:19:16 +0300

    Добавление валидации по наличию скриншота

--- autoload/models/form.php
+++ autoload/models/form.php
@@ -5,10 +5,12 @@ class form {
     public static function validate_post($f3)
     {
         $data = $f3->get('POST');
+        $files = $f3->get('FILES');
         if (($data[firstname] < 0) || ($data[firstname] > 1024) ||
             ($data[lastname] < 0) || ($data[lastname] > 1024) ||
             ($data[github] < 0) || ($data[github] > 1024) ||
-            ($data[telegram] < 0) || ($data[telegram] > 1024))
+            ($data[telegram] < 0) || ($data[telegram] > 1024) ||
+            (!isset($files[screenshot])))
         {
             return FALSE;
         }

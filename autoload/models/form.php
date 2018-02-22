4d9ea553b7d2d2e3ef68d2d560df9222984a92d5
wanku-x <wanku@bk.ru>
2018-02-23 01:03:31 +0300

    Добавление валидации по длине полей

--- autoload/models/form.php
+++ autoload/models/form.php
@@ -5,11 +5,21 @@ class form {
     public static function validate_post($f3)
     {
         $data = $f3->get('POST');
-        return TRUE;
+        if (($data[firstname] < 0) || ($data[firstname] > 1024) ||
+            ($data[lastname] < 0) || ($data[lastname] > 1024) ||
+            ($data[github] < 0) || ($data[github] > 1024) ||
+            ($data[telegram] < 0) || ($data[telegram] > 1024))
+        {
+            return FALSE;
+        }
+        else
+        {
+            return TRUE;
+        }
     }
 
     public static function create_record($f3)
     {
-        print_r($f3->get('POST'));
+        print_r($f3->get('FILES'));
     }
 }

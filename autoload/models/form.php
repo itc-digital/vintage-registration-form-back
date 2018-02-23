10a847346ad728e9d83d5497b726dd1a621f9f01
Mikhail Malyshev <mlshv42@ya.ru>
2018-02-24 00:24:26 +0300

    frontend -> backend

--- autoload/models/form.php
+++ autoload/models/form.php
@@ -6,7 +6,7 @@ class form {
     {
         $info = new \SplFileInfo($files[screenshot][name]);
         $file_name = md5(microtime()).'.'.$info->getExtension(); 
-        $dir = "/home/s/slink21/front/public_html/".$destination;
+        $dir = "/home/s/slink21/back/public_html/".$destination;
         move_uploaded_file($files[screenshot][tmp_name], $dir.$file_name);
         return $file_name;
     }
@@ -41,7 +41,7 @@ class form {
         $dir = 'ui/photos/';
         $file_name = form::save_screenshot($files, $dir);
 
-        $f3->get('DB')->exec('INSERT INTO `users`(`id`, `firstname`, `lastname`, `vk`, `github`, `telegram`, `faculty`, `screenshot`) VALUES (?,?,?,?,?,?,?,?)', 
+        $f3->get('DB')->exec('INSERT INTO `users_back`(`id`, `firstname`, `lastname`, `vk`, `github`, `telegram`, `faculty`, `screenshot`) VALUES (?,?,?,?,?,?,?,?)', 
 		array(
             1=> NULL,
 			2=> $data[firstname],
@@ -50,7 +50,7 @@ class form {
 			5=> $data[github],
             6=> $data[telegram],
             7=> $data[faculty],
-			8=> 'http://front.the-center.it/'.$dir.$file_name
+			8=> 'http://back.the-center.it/'.$dir.$file_name
 		));
     }
 }

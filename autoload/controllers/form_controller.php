476507231d4ea520471f5292ce2b79844ead7483
wanku-x <wanku@bk.ru>
2018-02-23 14:35:02 +0300

    Проверка краша

--- autoload/controllers/form_controller.php
+++ autoload/controllers/form_controller.php
@@ -5,11 +5,9 @@ class form_controller {
     public function handle_data($f3)
     {
         if (\models\form::validate_post($f3)) {
-            //\models\form::create_record($f3);
+            \models\form::create_record($f3);
             $f3->status(200);
         } else {
-            print_r($f3->get('POST'));
-            print_r($f3->get('FILES'));
             $f3->status(418);
         }
     }

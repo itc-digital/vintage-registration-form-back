ab9961411bcb122ae0dc3c4800de45cc7fcb3208
wanku-x <wanku@bk.ru>
2018-02-23 13:27:06 +0300

    Убираю проверку

--- autoload/controllers/form_controller.php
+++ autoload/controllers/form_controller.php
@@ -5,8 +5,7 @@ class form_controller {
     public function handle_data($f3)
     {
         if (\models\form::validate_post($f3)) {
-            print_r($f3->get('POST')[faculty]);
-            //\models\form::create_record($f3);
+            \models\form::create_record($f3);
             $f3->status(200);
         } else {
             $f3->status(418);
